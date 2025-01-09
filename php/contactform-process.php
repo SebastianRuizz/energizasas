d811a6040fb292592a56512cd4f2352ce5c8284d
energizasas <sebastianruizhurtadoo@gmail.com>
2025-01-08 20:29:05 -0500

    ajuste de carrusel

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "sguyumus1@gmail.com";
+$EmailTo = "recursoshumanos.energiza@gmail.com";
 $Subject = "New message from Aria landing page";
 
 // prepare email body text
